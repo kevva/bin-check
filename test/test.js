d374b992420f3284f20b2bf6a74720b6965d2184
Kevin Martensson <kevinmartensson@gmail.com>
2014-02-07 10:09:59 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,20 @@
+/*global describe, it */
+'use strict';
+
+var assert = require('assert');
+var binCheck = require('../');
+var path = require('path');
+
+describe('binCheck()', function () {
+    it('should test a binary and return true', function (cb) {
+        var bin = path.join(__dirname, 'fixtures/optipng-linux');
+
+        if (process.platform === 'darwin') {
+            bin = path.join(__dirname, 'fixtures/optipng-osx');
+        }
+
+        binCheck(bin, function (err, works) {
+            cb(assert.equal(works, true));
+        });
+    });
+});
