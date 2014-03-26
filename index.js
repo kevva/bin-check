698c4828766f432afdd24b29332493a6424e965e
Kevin Martensson <kevinmartensson@gmail.com>
2014-03-26 11:48:21 +0100

    Fix Windows spawning

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var executable = require('executable');
-var spawn = require('child_process').spawn;
+var spawn = process.platform === 'win32' ? require('win-spawn') : require('child_process').spawn;
 
 /**
  * Check if a executable is working correctly by checking it's exit code
