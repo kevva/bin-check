b1e9a6c4d365d8a97c57273c78ff0b8b5b948b54
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-16 18:14:17 +0200

    Remove `.sync()` method
    Fixes #3.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var path = require('path');
 var test = require('ava');
 var binCheck = require('../');
@@ -19,17 +18,4 @@ test('test a binary and return true', function (t) {
 		t.assert(!err, err);
 		t.assert(works, works);
 	});
-});
-
-test('test a binary and return true using sync', function (t) {
-	var bin = path.join(__dirname, 'fixtures/optipng-osx');
-
-	if (process.platform === 'darwin') {
-		bin = path.join(__dirname, 'fixtures/optipng-osx');
-	} else if (process.platform === 'win32') {
-		bin = path.join(__dirname, 'fixtures/optipng-win32.exe');
-	}
-
-	t.assert(binCheck.sync(bin));
-	t.end();
 });
